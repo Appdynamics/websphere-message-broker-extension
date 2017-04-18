4c651d945dcfc95bdc92b08aaabfac1c70a0596d
Kunal Gupta <kunal.gupta@appdynamics.com>
2017-04-18 16:56:29 -0700

    script changes

--- src/main/resources/appd-message-broker-monitor.bat
+++ src/main/resources/appd-message-broker-monitor.bat
@@ -1,4 +1,4 @@
 @echo off
 
-java -cp "./*;..\..\machineagent.jar;lib\*" -Dlog4j.configuration=.\log4j.xml -Dextension.configuration=file:.\monitors\WMBMonitor\config.yml com.appdynamics.extensions.wmb.WMBMonitor
+java -cp "./*;..\..\machineagent.jar;lib\*" -Dlog4j.configuration=.\log4j.xml -Dextension.configuration=.\monitors\WMBMonitor\config.yml com.appdynamics.extensions.wmb.WMBMonitor
 
