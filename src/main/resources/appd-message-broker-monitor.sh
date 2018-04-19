ba96440659ce6ba4dc0046f1713d523a1c082f7e
BalakrishnaV <balakrishna.v@appdynamics.com>
2018-04-19 18:38:23 +0530

    Extension not working if spaces in path to lib (Fixed by enclosing in quotes #130218)

--- src/main/resources/appd-message-broker-monitor.sh
+++ src/main/resources/appd-message-broker-monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #Passing machine agent process id to java process.
-../../jre/bin/java -cp "./*:../../machineagent.jar:/opt/mqm/java/lib/*" -Djava.library.path=/opt/mqm/java/lib64 -Dlog4j.configuration=file:./log4j.xml -Dextension.configuration=./monitors/WMBMonitor/config.yml com.appdynamics.extensions.wmb.WMBMonitor $PPID
+../../jre/bin/java -cp "./*:../../machineagent.jar:/opt/mqm/java/lib/*" -Djava.library.path="/opt/mqm/java/lib64" -Dlog4j.configuration=file:./log4j.xml -Dextension.configuration=./monitors/WMBMonitor/config.yml com.appdynamics.extensions.wmb.WMBMonitor $PPID
 
 exit $?
